762a588c6e13581c88c282c1e6c10a757734597f
baocvcv <baocvcv@gmail.com>
2020-04-02 23:20:09 +0800

    Finish add pinyin to word

--- a1-pinyin/src/ngram.py
+++ a1-pinyin/src/ngram.py
@@ -5,6 +5,8 @@ from os import makedirs
 from os.path import join, isdir, exists
 from math import log
 from collections import defaultdict, Counter, OrderedDict
+import time
+import re
 
 import dill as pickle
 from pypinyin import lazy_pinyin, load_phrases_dict
@@ -47,12 +49,15 @@ class NGramModel(BaseModel):
     def train(self, force=False):
         ' Train '
         print("[Info] Training the model...")
+        t = time.time()
         for i in range(self.n-1):
             if force or not exists(join(self.model_dir, 'prob%d.p' % i)):
                 print("[Info] Running with n = %d" % i)
                 self.train_n(i)
         print("[Info] Running with n = %d" % (self.n - 1))
         self.train_n(self.n-1)
+        t = round(time.time() - t, 3)
+        print("[Info] Training took %s seconds" % (t))
 
     def train_n(self, nn):
         ' Train '
@@ -97,7 +102,6 @@ class NGramModel(BaseModel):
             if _len-nn >= 0:
                 # p = line[_len-nn : _len]
                 phrase_counter[get_line(_len-nn, _len)] += 1
-
 
         # print("华|新: ", conditional_pro[index['新xin']][self.all_words['华hua']])
         # print("新：", phrase_counter['新xin'])
@@ -151,14 +155,15 @@ class NGramModel(BaseModel):
 
     def translate(self, pinyin_input: str) -> str:
         ' Translate the input pinyin to words '
-        pinyin_input = pinyin_input.lower().split(' ')
+        pinyin_input = pinyin_input.lower().strip()
+        pinyin_input = re.split(r'\s+', pinyin_input)
 
         old_sentences = defaultdict(float)
         old_sentences[''] = .0
         for _len, syllable in enumerate(pinyin_input):
             # For each pinyin, get candidate words
             # Calculate conditional probability, record history
-            print('[%d]: ' % _len, old_sentences)
+            # print('[%d]: ' % _len, old_sentences)
             new_sentences = defaultdict(float)
             for w in self.pinyin_dict[syllable]:
                 best_sentence = ''
@@ -166,7 +171,7 @@ class NGramModel(BaseModel):
                 for sentence in old_sentences:
                     w_prev = sentence[-self.n+1:]
                     prob = self.get_probability(w + pinyin_input[_len], w_prev,
-                                                pinyin_input[_len-self.n:_len])
+                                                pinyin_input[_len-self.n+1:_len])
                     cur_prob = old_sentences[sentence] + log(prob)
                     if cur_prob > max_prob:
                         max_prob = cur_prob
@@ -177,7 +182,7 @@ class NGramModel(BaseModel):
         # sort result
         result = list(old_sentences.items())
         result.sort(key=lambda r: r[1], reverse=True)
-        print(result[:5])
+        # print(result[:5])
         if len(result) == 0:
             print("[Error] Please check your input.")
             return ''
@@ -185,6 +190,7 @@ class NGramModel(BaseModel):
 
     def get_probability(self, w: str, w_prev: str, prev_py: list):
         ' Retrieve probability of P(w | w_prev) '
+        # print(w, w_prev, prev_py)
         if w_prev == '':
             # the index for '' is 0
             idx_word = self.all_words[w]
@@ -194,7 +200,7 @@ class NGramModel(BaseModel):
                 return self.lambdas[0][0]
 
         l = len(w_prev)
-        w_prev_py = [w_prev[i]+py for i,py in enumerate(prev_py)]
+        w_prev_py = ''.join([w_prev[i]+py for i,py in enumerate(prev_py)])
         if w_prev_py in self.index[l]:
             idx = self.index[l][w_prev_py]
             idx_word = self.all_words[w]
