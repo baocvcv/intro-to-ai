cd5aeaf956b54ebd615dfb069b4a18a6a729d512
baocvcv <baocvcv@gmail.com>
2020-04-22 23:26:02 +0800

    Need to use noX, noY info

--- a2-connect4/src/Strategy/test.cpp
+++ a2-connect4/src/Strategy/test.cpp
@@ -2,27 +2,37 @@
 #include "Strategy.h"
 #include "Point.h"
 
+int s[100] = {
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+
+    0, 0, 0, 0, 0,  0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0,  0, 1, 0, 0, 0,
+    0, 0, 0, 0, 2,  0, 1, 0, 0, 0,
+    0, 1, 0, 0, 1,  0, 2, 0, 0, 0,
+    1, 1, 2, 1, 2,  2, 1, 0, 0, 0,
+};
+
+int top[10];
+
 int main() {
     int M = 10;
     int N = 10;
-    int s[100];
-    int top[10];
-    for (int j = 0; j < 10; j++) {
-        top[j] = 10;
-        for (int i = 0; i < 10; i++) {
-            s[i * 10 + j] = 0;
-        }
+
+    for (int j = 0; j < N; j++) {
+        int i;
+        for (i = 0; i < N && s[i*10+j] == 0; i++) ;
+        top[j] = i;
     }
 
-    // no moves on board
-    Point* p = getPoint(M, N, top, s, -1, -1, 5, 5);
-    std::cout << p->x << ' ' << p->y << '\n';
+    int x = 7;
+    int y = 6;
 
-    s[9 * 10 + 0] = 1;
-    s[9 * 10 + 7] = 2;
-    top[0] = 9;
-    top[7] = 9;
-    p = getPoint(M, N, top, s, 9, 0, 2, 9);
+    // no moves on board
+    Point* p = getPoint(M, N, top, s, x, y, 0, 0);
     std::cout << p->x << ' ' << p->y << '\n';
 
     return 0;
