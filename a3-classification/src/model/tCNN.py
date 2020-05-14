39de87a2b6d25b0cef81d929db2d65507eaae7f6
baocvcv <baocvcv@gmail.com>
2020-05-14 14:57:31 +0800

    Add requirements

--- a3-classification/src/model/tCNN.py
+++ a3-classification/src/model/tCNN.py
@@ -7,50 +7,6 @@ import numpy as np
 
 from .base_config import BaseConfig
 
-# class Config(object):
-
-#     """配置参数"""
-#     def __init__(self, dataset, embedding):
-#         self.model_name = 'tCNN'
-#         self.dataset_path = join('../data/', dataset)
-#         self.train_path = join(self.dataset_path, 'train.txt')
-#         self.dev_path = join(self.dataset_path, 'validate.txt')
-#         self.test_path = join(self.dataset_path, 'sinanews.test')
-#         self.class_list = [x.strip() for x in open(
-#             join(self.dataset_path, 'classes.txt'), encoding='utf-8').readlines()]
-#         self.vocab_path = join(self.dataset_path, 'vocab_dict.pkl')
-#         self.save_path = join(self.dataset_path,
-#                               'saved_model/',
-#                               self.model_name + '.ckpt')
-#         self.log_path = join(self.dataset_path, 'log', self.model_name)
-#         if embedding != 'random':
-#             self.embedding_pretrained = torch.tensor(
-#                 np.load(join(self.dataset_path, embedding))["embeddings"].astype('float32'))
-#         else:
-#             self.embedding_pretrained = None
-#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
-
-#         ''' general training params '''
-#         self.dropout = 0.5
-#         # if error rate does not improve over #require_improvment epochs, stop training
-#         self.require_improvement = 1000
-#         self.num_classes = len(self.class_list)
-#         # vocab dict length
-#         self.n_vocab = 0
-#         self.num_epochs = 10 #TODO for debug
-#         self.batch_size = 32
-#         # sentence length
-#         self.pad_size = 512
-#         self.learning_rate = 1e-3
-#         self.embed = self.embedding_pretrained.size(1)\
-#             if self.embedding_pretrained is not None else 300
-
-#         ''' model specific params '''
-#         # kernel sizes for the first layer
-#         self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
-#         # kernel number for the first layer
-#         self.num_filters = 256                                          # 卷积核数量(channels数)
-
 class Config(BaseConfig):
     """ Model configs """
     
--- a3-classification/src/model/tRNN.py
+++ a3-classification/src/model/tRNN.py
@@ -6,52 +6,6 @@ import numpy as np
 
 from .base_config import BaseConfig
 
-# class Config(object):
-#     """ Model configs """
-
-#     def __init__(self, dataset, embedding):
-#         ''' all the parameters '''
-
-#         ''' paths and devices '''
-#         self.model_name = 'tRNN'
-#         self.dataset_path = join('../data/', dataset)
-#         self.train_path = join(self.dataset_path, 'train.txt')
-#         self.dev_path = join(self.dataset_path, 'validate.txt')
-#         self.test_path = join(self.dataset_path, 'sinanews.test')
-#         self.class_list = [x.strip() for x in open(
-#             join(self.dataset_path, 'classes.txt'), encoding='utf-8').readlines()]
-#         self.vocab_path = join(self.dataset_path, 'vocab_dict.pkl')
-#         self.save_path = join(self.dataset_path,
-#                               'saved_model/',
-#                               self.model_name + '.ckpt')
-#         self.log_path = join(self.dataset_path, 'log', self.model_name)
-#         if embedding != 'random':
-#             self.embedding_pretrained = torch.tensor(
-#                 np.load(join(self.dataset_path, embedding))["embeddings"].astype('float32'))
-#         else:
-#             self.embedding_pretrained = None
-#         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
-
-
-#         ''' general training params '''
-#         self.dropout = 0.5
-#         # if error rate does not improve over #require_improvment epochs, stop training
-#         self.require_improvement = 1000
-#         self.num_classes = len(self.class_list)
-#         # vocab dict length
-#         self.n_vocab = 0
-#         self.num_epochs = 10 #TODO for debug
-#         self.batch_size = 32
-#         # sentence length
-#         self.pad_size = 512
-#         self.learning_rate = 1e-3
-#         self.embed = self.embedding_pretrained.size(1)\
-#             if self.embedding_pretrained is not None else 300
-
-#         ''' model specific params '''
-#         self.hidden_size = 128                                          # lstm隐藏层
-#         self.num_layers = 2                                             # lstm层数
-
 class Config(BaseConfig):
     """ Model configs """
     
